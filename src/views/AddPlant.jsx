35c3d02abe4243d378116c1a04e8100001429a1e
Amie Perkins <95150144+amielau@users.noreply.github.com>
2022-05-24 17:51:22 -0600

    fleshed out the addPlants component, still need to do a lot of debugging on it though

--- src/views/AddPlant.jsx
+++ src/views/AddPlant.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { DatePicker, Form, Input, Radio, Select, Switch } from 'antd'
 import { styled } from '@mui/material/styles'
 import { Button } from '@mui/material'
 
-function AddPlant() {
+const AddPlant = () => {
+  const [componentSize, setComponentSize] = useState('default')
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size)
+  }
   return (
-    <div>
-      <form>
-        <input></input>
-        <input></input>
-      </form>
-    </div>
+    <Form
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 14,
+      }}
+      layout='horizontal'
+      initialValues={{
+        size: componentSize,
+      }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize}
+    >
+      <Form.Item label='Plant Name'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='NickName'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='Extra Care Instructions'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='Select'>
+        <Select>
+          <Select.Option value='demo'>Demo</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label='DatePicker'>
+        <DatePicker />
+      </Form.Item>
+      <Form.Item label='Daily Watering Reminder?' valuePropName='checked'>
+        <Switch />
+      </Form.Item>
+      <Form.Item>
+        <Button>Submit</Button>
+      </Form.Item>
+    </Form>
   )
 }
 
