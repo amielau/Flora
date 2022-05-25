a8b211cd905783cdd77731fa49c424b27ec138e5
Amie Perkins <95150144+amielau@users.noreply.github.com>
2022-05-25 16:44:00 -0600

    added to AddPlant

--- src/views/AddPlant.jsx
+++ src/views/AddPlant.jsx
@@ -1,22 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { styled } from '@mui/material/styles'
-import { Button, Box } from '@mui/material'
-import DateTimePicker from '@mui/lab/DateTimePicker'
+import { Box, Button, Container, Stack, TextField, Switch } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
 
 const AddPlant = () => {
-  const [componentSize, setComponentSize] = useState('default')
+  const { handleSubmit, control } = useForm({
+    defaultValues: { type: 'Plant type', nickname: 'Nickname', careInst: 'Care Instructions' },
+  })
+  const attemptSubmit = handleSubmit(values => {
+    console.log('values', values)
+  })
 
-  // select image //
-  // const [selectedImage, setSelectedImage] = useState(null)
-  // const [imageUrl, setImageUrl] = useState(null)
-  // useEffect(() => {
-  //   if (selectedImage) {
-  //     setImageUrl(URL.createObjectURL(selectedImage))
-  //   }
-  // }, [selectedImage])
-  // select image //
-
-  return
+  return (
+    <Container>
+      <Box />
+      <Stack spacing={2}>
+        <Controller
+          name=''
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <TextField label='Plant Type' {...field} />}
+        />
+        <Controller
+          name=''
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <TextField label='Nickname' {...field} />}
+        />
+        <Controller
+          name=''
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <TextField label='Care Instructions' {...field} />}
+        />
+        <Controller
+          name='watering toggle'
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <Switch label='Water Daily?' {...field} />}
+        />
+      </Stack>
+      <Button variant='contained' onClick={attemptSubmit}>
+        {' '}
+        Login
+      </Button>
+    </Container>
+  )
 }
 
 export default AddPlant
