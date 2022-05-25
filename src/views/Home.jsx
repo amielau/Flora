03b88e637484a4064e4f9c397f445b21f39d32c3
Amie Perkins <95150144+amielau@users.noreply.github.com>
2022-05-25 16:14:33 -0600

    got rid of craco

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,39 @@ import { Button, Box } from '@mui/material'
 import capstoneVid from '../assets/capstoneVid.mp4'
 
 // words arr self-typing
-const words = ['test1', 'test2']
+const words = [
+  'Have you botany plants lately?',
+  'Someone has been adding soil to my garden. The plot thickens',
+  'Im very frond of you.',
+  'Scarecrows are always garden their patch',
+]
 
 function Home() {
   const [index, setIndex] = useState(0)
   const [subIndex, setSubIndex] = useState(0)
   const [reverse, setReverse] = useState(false)
+
+  // typeWriter
+  useEffect(() => {
+    if (index === words.length - 1 && subIndex === words[index].length) {
+      return
+    }
+    if (subIndex === words[index].length + 1 && index !== words.length - 1 && !reverse) {
+      setReverse(true)
+      return
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false)
+      setIndex(prev => prev + 1)
+      return
+    }
+    const timeout = setTimeout(() => {
+      setSubIndex(prev => prev + (reverse ? -1 : 1))
+    }, 200)
+
+    return () => clearTimeout(timeout)
+  }, [subIndex, index, reverse])
 
   return (
     <div className='Home'>
