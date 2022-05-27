0989cd5374e937f3501037fe72ad77aa018d4b93
Paul Jones <24881961+pvjones@users.noreply.github.com>
2022-05-26 23:44:23 -0600

    adding image

--- src/views/Care.jsx
+++ src/views/Care.jsx
@@ -1,7 +1,15 @@
+import { Box } from '@mui/material'
 import React from 'react'
-import { Box, Button, Checkbox } from '@mui/material'
 
 function Care() {
-  return <Checkbox></Checkbox>
+  return (
+    <Box sx={{ display: 'flex', flex: '1', height: '100%' }}>
+      <img
+        src='https://www.dropbox.com/s/9hvrhst7vfye4q1/Screen%20Shot%202022-05-26%20at%2011.34.03%20PM.jpeg?raw=1'
+        alt='status-1'
+        style={{ maxHeight: '100%', maxWidth: '100%' }}
+      />
+    </Box>
+  )
 }
 export default Care
